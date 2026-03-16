d99f429902e1bd0308672aaa861c304cf41726a5
stevenstank <stevenstank775@gmail.com>
2026-03-16 15:13:24 +0000

    learned ejs

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
+
 const express = require("express");
-const app = express();
+const path = require("node:path");
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+const app = express(); 
 
-const PORT = 3000;
-app.listen(PORT, (error) => {
-  // This is important!
-  // Without this, any startup errors will silently fail
-  // instead of giving you a helpful error message.
-  if (error) {
-    throw error;
-  }
-  console.log(`My first Express app - listening on port ${PORT}!`);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+app.get("/", (req, res) => {
+  res.render("index", { links: links, users: users });
+
+  // app.js
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+});
+
+app.listen(3000, () => {           
+  console.log("Server running on http://localhost:3000/");
 });
